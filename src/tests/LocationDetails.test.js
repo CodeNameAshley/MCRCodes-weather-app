2044242567d08af2345f9cfff5dc952ed928f210
ashleyn <ashleynavo@googlemail.com>
2022-02-13 16:12:55 +0000

    updated test

--- src/tests/LocationDetails.test.js
+++ src/tests/LocationDetails.test.js
@@ -5,7 +5,11 @@ import LocationDetails from "../components/LocationDetails";
 describe("LocationDetails", () => {
   it("renders the correct city and location props", () => {
     const { getByText } = render(
-      <LocationDetails city="Bikini Bottom" country="Pacific Ocean" />
+      <LocationDetails
+        className="location-details"
+        city="Bikini Bottom"
+        country="Pacific Ocean"
+      />
     );
 
     expect(getByText("Bikini Bottom, Pacific Ocean")).toHaveClass(
--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { getByTestId, render } from "@testing-library/react";
+import ForecastSummary from "../../components/ForecastSummary";
 
 describe("ForecastSummary", () => {
   const validProps = {
-    date: 123456,
+    date: 1111111,
     description: "Stub description",
     icon: "stubIcon",
     temperature: {
@@ -12,7 +13,7 @@ describe("ForecastSummary", () => {
     },
   };
 
-  it("renders a summary of the forecast", () => {
+  it("renders correctly", () => {
     const { asFragment } = render(
       <ForecastSummary
         date={validProps.date}
@@ -23,5 +24,23 @@ describe("ForecastSummary", () => {
     );
 
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("renders correct values for props", () => {
+    const { getByText } = render(
+      <ForecastSummary
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />
+    );
+
+    expect(getByText("1111111")).toHaveClass("forecast-summary__date");
+    expect(getByText("Stub description")).toHaveClass(
+      "forecast-summary__description"
+    );
+    expect(getByTestId("forecast-icon")).toHaveClass("forecast-summary__icon");
+    expect(getByText("22°C")).toHaveClass("forecast-summary__temperature");
   });
 });
